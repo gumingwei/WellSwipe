3715ab0a63299b069f54f3dafead92635a3c6b91
mingwei <721881283@qq.com>
2016-05-10 18:12:43 +0800

    home,recently,锁屏,samsung

--- app/src/main/java/com/well/swipe/service/SwipeService.java
+++ app/src/main/java/com/well/swipe/service/SwipeService.java
@@ -21,11 +21,13 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.well.swipe.BootReceiver;
 import com.well.swipe.ItemApplication;
 import com.well.swipe.LauncherModel;
 import com.well.swipe.R;
@@ -113,6 +115,8 @@ public class SwipeService extends Service implements CatchView.OnEdgeSlidingList
 
     private Tracker mTracker;
 
+    private HomeWatchReceiver mWatchRecevier = new HomeWatchReceiver();
+
     IBinder mBinder = new ServiceBind();
 
     public class ServiceBind extends Binder {
@@ -129,7 +133,7 @@ public class SwipeService extends Service implements CatchView.OnEdgeSlidingList
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Log.i("Gmw", "SwipeService_onCreate");
         //Google
         SwipeApplication application = (SwipeApplication) getApplication();
         mTracker = application.getDefaultTracker();
@@ -202,6 +206,12 @@ public class SwipeService extends Service implements CatchView.OnEdgeSlidingList
                 .getUriFor(Settings.System.SCREEN_BRIGHTNESS), false, mObserver);
         getContentResolver().registerContentObserver(Settings.System
                 .getUriFor(Settings.System.SCREEN_BRIGHTNESS_MODE), false, mObserver);
+
+        /**
+         * home监听
+         */
+        final IntentFilter homeFilter = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        registerReceiver(mWatchRecevier, homeFilter);
     }
 
     @Nullable
@@ -229,6 +239,7 @@ public class SwipeService extends Service implements CatchView.OnEdgeSlidingList
         mCatchViewRight2.dismiss();
         unregisterReceiver(mReceiver);
         getContentResolver().unregisterContentObserver(mObserver);
+        unregisterReceiver(mWatchRecevier);
 
     }
 
@@ -775,6 +786,43 @@ public class SwipeService extends Service implements CatchView.OnEdgeSlidingList
             super.onChange(paramBoolean);
             mSwipeLayout.getAngleLayout().getAngleView().refreshToolsView();
             mSwipeLayout.getAngleLayout().getAngleView().requestLayout();
+        }
+    }
+
+    class HomeWatchReceiver extends BootReceiver {
+
+        private static final String TAG = "HomeWatchReceiver";
+
+        private static final String SYSTEM_DIALOG_REASON_KEY = "reason";
+
+        private static final String SYSTEM_DIALOG_REASON_RECENT_APPS = "recentapps";
+
+        private static final String SYSTEM_DIALOG_REASON_HOME_KEY = "homekey";
+
+        private static final String SYSTEM_DIALOG_REASON_LOCK = "lock";
+
+        private static final String SYSTEM_DIALOG_REASON_ASSIST = "assist";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            super.onReceive(context, intent);
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)) {
+                String reason = intent.getStringExtra(SYSTEM_DIALOG_REASON_KEY);
+                if (SYSTEM_DIALOG_REASON_HOME_KEY.equals(reason)) {
+                    // 短按Home键
+                    mSwipeLayout.getAngleLayout().off();
+                } else if (SYSTEM_DIALOG_REASON_RECENT_APPS.equals(reason)) {
+                    // 长按Home键 或者 activity切换键
+                    mSwipeLayout.getAngleLayout().off();
+                } else if (SYSTEM_DIALOG_REASON_LOCK.equals(reason)) {
+                    // 锁屏
+                    mSwipeLayout.getAngleLayout().off();
+                } else if (SYSTEM_DIALOG_REASON_ASSIST.equals(reason)) {
+                    // samsung 长按Home键
+                    mSwipeLayout.getAngleLayout().off();
+                }
+            }
         }
     }
 
--- app/src/main/java/com/well/swipe/view/AngleLayout.java
+++ app/src/main/java/com/well/swipe/view/AngleLayout.java
@@ -4,7 +4,6 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
@@ -260,14 +259,14 @@ public class AngleLayout extends FrameLayout implements AngleView.OnAngleChangeL
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mWidth = getMeasuredWidth();
         mHeight = getMeasuredHeight();
-        Log.i("Gmw", "AngleLayout_onMeasure()");
+
     }
 
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        Log.i("Gmw", "AngleLayout_onSizeChanged()");
+
     }
 
     @Override
