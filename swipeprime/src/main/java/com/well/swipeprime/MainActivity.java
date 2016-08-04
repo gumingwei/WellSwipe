5ccddfeb26ba404ba7a6efb6b8e6e4a8a6907821
mingwei <721881283@qq.com>
2016-08-04 23:28:14 +0800

    add->swipeprime app

--- swipeprime/src/main/java/com/well/swipeprime/MainActivity.java
+++ swipeprime/src/main/java/com/well/swipeprime/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
